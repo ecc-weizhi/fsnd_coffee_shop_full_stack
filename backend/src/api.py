fb8fe4243a632c17535e338a5031446f88e38d76
ecc-weizhi <ecc.weizhi@gmail.com>
2021-07-04 12:55:35 -0700

    Implement skeleton for api.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -17,73 +17,84 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
+
 
 # ROUTES
-'''
-@TODO implement endpoint
+@app.route("/drinks", methods=["GET"])
+def get_drinks():
+    """
+    @TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+    :return: status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    or appropriate status code indicating reason for failure
+    """
+    pass
 
 
-'''
-@TODO implement endpoint
+@app.route("/drinks-detail", methods=["GET"])
+def get_drinks_detail():
+    """
+    @TODO implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+    :return: status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    or appropriate status code indicating reason for failure
+    """
+    pass
 
 
-'''
-@TODO implement endpoint
+@app.route("/drinks", methods=["POST"])
+def post_drinks():
+    """
+    @TODO implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+    :return: status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+    or appropriate status code indicating reason for failure
+    """
+    pass
 
 
-'''
-@TODO implement endpoint
+@app.route("/drinks/<int:id>", methods=["PATCH"])
+def patch_drinks(id):
+    """
+    @TODO implement endpoint
     PATCH /drinks/<id>
-        where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+    :param id: existing model id
+    :return: status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+    or appropriate status code indicating reason for failure
+    """
+    pass
 
 
-'''
-@TODO implement endpoint
+@app.route("/drinks/<int:id>", methods=["DELETE"])
+def delete_drinks(id):
+    """
+    @TODO implement endpoint
     DELETE /drinks/<id>
-        where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-
-
-# Error Handling
-'''
-Example error handling for unprocessable entity
-'''
+    :param id: existing model id
+    :return: status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+    or appropriate status code indicating reason for failure
+    """
+    pass
 
 
 @app.errorhandler(422)
 def unprocessable(error):
+    """Example error handling for unprocessable entity"""
     return jsonify({
         "success": False,
         "error": 422,
@@ -102,13 +113,13 @@ def unprocessable(error):
 
 '''
 
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
+
+@app.errorhandler(404)
+def not_found(error):
+    """@TODO implement error handler for 404"""
+    pass
 
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
+def auth_error(error):
+    """@TODO implement error handler for AuthError"""
+    pass
