b15498e8d11f2e668e9a9067f02a76d949266e71
ecc-weizhi <ecc.weizhi@gmail.com>
2021-07-06 23:27:38 -0700

    Implement post_drinks

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,9 +1,14 @@
 import os
+from json import JSONDecodeError
+
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+from sqlalchemy.exc import SQLAlchemyError
 
+from .utils.utils import check_fields_exist
+from .client_error_exceptions.client_error_exceptions import BadRequest, UnprocessableEntity
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
 
@@ -58,9 +63,9 @@ def get_drinks_detail():
 
 
 @app.route("/drinks", methods=["POST"])
-def post_drinks():
+@requires_auth("post:drinks")
+def post_drinks(jwt_payload):
     """
-    @TODO implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -68,7 +73,29 @@ def post_drinks():
     :return: status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
     or appropriate status code indicating reason for failure
     """
-    pass
+    data_string = request.data
+    try:
+        request_json = json.loads(data_string)
+    except JSONDecodeError:
+        raise BadRequest()
+
+    missing_field = check_fields_exist(request_json, ["title", "recipe"])
+    if missing_field:
+        raise UnprocessableEntity.missing_fields(missing_field)
+
+    try:
+        drink = Drink(title=request_json["title"], recipe=json.dumps(request_json["recipe"]))
+    except:
+        raise UnprocessableEntity()
+
+    try:
+        drink.insert()
+        return jsonify({
+            "success": True,
+            "drinks": [drink.long()],
+        })
+    except SQLAlchemyError:
+        abort(500)
 
 
 @app.route("/drinks/<int:id>", methods=["PATCH"])
